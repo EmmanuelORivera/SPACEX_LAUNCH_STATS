9e6062b58b74a54ea3774ae09b44ee076f2c62fe
Emmanuel Ochoa <eorivera95@gmail.com>
2020-09-07 18:40:56 -0600

    Final commit

---
+++ public/precache-manifest.0558320ad8e8f3ebe38287e7b1bf07fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706a791c0f31f926e40aa3e103881920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ced447971f9b1cb3c77",
+    "url": "/static/css/main.fca00966.chunk.css"
+  },
+  {
+    "revision": "76e6a3c3be66e0e07f8d",
+    "url": "/static/js/2.50e82398.chunk.js"
+  },
+  {
+    "revision": "2593e898aee047a7f0ff285a7a5ee26c",
+    "url": "/static/js/2.50e82398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ced447971f9b1cb3c77",
+    "url": "/static/js/main.a983e3c7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "dd0b9db22beba6527158325ae8077761",
+    "url": "/static/media/logo.dd0b9db2.PNG"
+  }
+]);
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const schema = require('./schema');
+const path = require('path');
+
 const app = express();
+
+app.use(cors());
 
 app.use(
   '/graphql',
@@ -11,6 +17,12 @@ app.use(
     graphiql: true,
   })
 );
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
